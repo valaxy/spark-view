6e4a99c0007b935e1d67221b372c48aeab3fe36b
tian <valaxy.tian@gmail.com>
2015-06-01 07:53:20 +0000

    add cloud9 project

---
+++ .cloud9/index.js
@@ -0,0 +1,20 @@
+var express = require('express'),
+    jadeStatic = require('jade-static'),
+    sassStatic = require('express-compile-sass')
+    path = require('path')
+    
+    
+var app = express()
+
+app.use(
+    '/', 
+    jadeStatic(path.join(__dirname, '../')),
+    sassStatic({
+        root: '../'
+    }),
+    express.static('../')
+)
+
+app.listen(process.env.PORT, process.env.IP)
+
+    
